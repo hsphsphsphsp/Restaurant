f54669975cd7b095caf93c3aba45737e434484b7
hsp <hsp8405@naver.com>
2024-05-20 14:05:35 +0900

    [feature] add exception test

--- Sample-Test1/RestraurantTest.cpp
+++ Sample-Test1/RestraurantTest.cpp
@@ -1,8 +1,25 @@
-#include "gtest/gtest.h"
+﻿#include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../Restraurant/BookingScheduler.cpp"
+#include <ctime>
+#include <stdexcept>
 
-TEST(TestCaseName, TestName) {
-	BookingScheduler sc(5);
+TEST(BookingSchedulerTest, Exception) {
+	tm notOnTheHour = { 0 };
+	notOnTheHour.tm_year = 2021 - 1900;
+	notOnTheHour.tm_mon = 03 - 1;
+	notOnTheHour.tm_mday = 26;
+	notOnTheHour.tm_hour = 9;
+	notOnTheHour.tm_min = 5;
+	notOnTheHour.tm_isdst = -1;
+	mktime(&notOnTheHour);
+
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	EXPECT_THROW({
+		bookingScheduler.addSchedule(schedule);
+		}, std::runtime_error);
 	EXPECT_EQ(1, 1);
 }
